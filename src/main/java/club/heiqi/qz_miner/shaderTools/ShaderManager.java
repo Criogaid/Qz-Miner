de5cff455dc3d6c93ddd13ae52f31133811ae5c1
Kripto <932593801@qq.com>
2026-02-25 15:00:21 +0800

    perf: 复用矩阵缓冲避免uniform上传临时数组分配

--- src/main/java/club/heiqi/qz_miner/shaderTools/ShaderManager.java
+++ src/main/java/club/heiqi/qz_miner/shaderTools/ShaderManager.java
@@ -141,7 +141,7 @@ public class ShaderManager {
         int location = getUniformLocation(name);
         if (location != -1) {
             matrixBuffer.clear();
-            matrixBuffer.put(value.get(new float[16]));
+            value.get(matrixBuffer);
             matrixBuffer.flip();
             GL20.glUniformMatrix4(location, false, matrixBuffer);
         }
